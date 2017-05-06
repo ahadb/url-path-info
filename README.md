5660165d40be5799c0704aae63ca5e58ab231f18
Ahad Bokhari <ahadbokhari@gmail.com>
2017-05-06 17:36:06 -0400

    inital commit & files for package

---
+++ README.md
@@ -0,0 +1,36 @@
+# url-path-name
+
+Package that shows path info in an easy to use object
+
+## Installation
+```javascript
+npm i url-path-info --save-dev
+```
+
+## Usage
+Just require the `url-path-info` module. 
+
+```javascript
+const pathInfo = require('./index');
+
+pathInfo('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Mapid?foo=lorem&bar=&baz');
+
+// => Output
+{ href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Mapid?foo=lorem&bar=&baz',
+  protocol: 'https:',
+  host: 'developer.mozilla.org',
+  params: 
+   { 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Mapid?foo': 'lorem',
+     bar: '',
+     baz: '' },
+  port: undefined,
+  pathname: '/en-US/docs/Web/JavaScript/Reference/Global_Objects/Mapid',
+  search: '?foo=lorem&bar=&baz',
+  hash: ''
+```
+
+## Tests
+
+```bash
+npm test
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ahadbokhari on 5/04/17.
+ */
+/**
+ * returns path info in an easy to use object format
+ * @param {str} string
+ * @return {obj}
+ */
+
+const getParams = (query) => {
+  if (!query) {
+    return { };
+  }
+
+  return (/^[?#]/.test(query) ? query.slice(1) : query)
+    .split('&')
+    .reduce((params, param) => {
+      let [ key, value ] = param.split('=');
+      params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+      return params;
+    }, { });
+};
+
+module.exports = (path) => {
+  if (!path) {
+    return new Error('You need to pass a reasonable url to the function');
+  }
+  const match = path.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+
+  return match && {
+    href: path,
+    protocol: match[1],
+    host: match[2],
+    params: getParams(path),
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
+
+  };
+};
+
+
+
+
